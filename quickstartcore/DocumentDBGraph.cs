6a9d712885b8a79bbf8a5f1fc2a504aaa20826f4
memory125 <mengde1120@sina.com>
2017-10-19 14:00:01 -0700

    Modified the file DocumentDBGraph.cs

--- quickstartcore/DocumentDBGraph.cs
+++ quickstartcore/DocumentDBGraph.cs
@@ -2,21 +2,23 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Configuration;
     using System.Linq;
     using System.Linq.Expressions;
     using System.Threading.Tasks;
     using Microsoft.Azure.Documents;
     using Microsoft.Azure.Documents.Client;
     using Microsoft.Azure.Documents.Linq;
+    using Microsoft.Azure.Graphs;
+    using Newtonsoft.Json;
+    using Microsoft.Azure.Graphs.Elements;
 
-    
     public static class DocumentDBGraph<T> where T : class
-    {       
-        private static readonly string Endpoint = "https://iotlabde-argonne-test.documents.azure.com:443/";
-        private static readonly string Key = "5d6sOZoULrtX2OYzZyNEt4avVArJc5JJq7M2Q7Pm7t6qVzj8gD3ovIqTCYCViTRay3XXJMom5u3oQnu7yZCtHA==";
-        private static readonly string DatabaseId = "ToDoList";
-        private static readonly string CollectionId = "Items";
+    {
+        private static readonly string DatabaseId = ConfigurationManager.AppSettings["database"];
+        private static readonly string CollectionId = ConfigurationManager.AppSettings["collection"];
         private static DocumentClient client;
+        private static DocumentCollection graph;
 
         public static async Task<T> GetItemAsync(string id)
         {
@@ -55,39 +57,133 @@
             return results;
         }
 
-        public static async Task<Document> CreateItemAsync(T item)
+        public static async Task<Vertex> InsertVertexAsync(string destVertex, Dictionary<string, string> dictionary)
         {
-            //foreach (KeyValuePair<string, string> gremlinQuery in dictionary)
-            //{
-            //    // The CreateGremlinQuery method extensions allow you to execute Gremlin queries and iterate
-            //    // results asychronously
-            //    IDocumentQuery<dynamic> query = client.CreateGremlinQuery<dynamic>(graph, gremlinQuery.Value);
-            //    while (query.HasMoreResults)
-            //    {
-            //        foreach (dynamic result in await query.ExecuteNextAsync())
-            //        {
-            //            Console.WriteLine($"\t {JsonConvert.SerializeObject(result)}");
-            //        }
-            //    }
+            Vertex result = null; 
+            string gremlinQuery = string.Format($"g.addV({destVertex})");            
+            foreach (KeyValuePair<string, string> property in dictionary)
+            {
+                string propertyString = string.Format($".property({property.Key}, {property.Value})");
+            }
+           
+            // gremlinQuery likes g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)
 
-            //    Console.WriteLine();
-            //}
-            return await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId), item);
+            Console.WriteLine($"Running {gremlinQuery}");
+
+            // The CreateGremlinQuery method extensions allow you to execute Gremlin queries and iterate
+            // results asychronously
+            IDocumentQuery<Vertex> addVertex = client.CreateGremlinQuery<Vertex>(graph, gremlinQuery);
+            while (addVertex.HasMoreResults)
+            {
+                foreach (Vertex vertex in await addVertex.ExecuteNextAsync<Vertex>())
+                {
+                    // Since Gremlin is designed for multi-valued properties, the format returns an array. Here we just read
+                    // the first value
+                    result = vertex;
+                    Console.WriteLine($"\t {JsonConvert.SerializeObject(vertex)}");
+                }
+            }
+            Console.WriteLine();
+
+            return await Task.FromResult<Vertex>(result);
         }
 
-        public static async Task<Document> UpdateItemAsync(string id, T item)
+        public static async void CleanUpVertexAsync()
         {
-            return await client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id), item);
+            string gremlinQuery = "g.V().drop()";
+            IDocumentQuery<Vertex> clrupVertex = client.CreateGremlinQuery<Vertex>(graph, gremlinQuery);
+            while (clrupVertex.HasMoreResults)
+            {
+                foreach (Vertex vertex in await clrupVertex.ExecuteNextAsync<Vertex>())
+                {
+                    // Since Gremlin is designed for multi-valued properties, the format returns an array. Here we just read
+                    // the first value
+                    Console.WriteLine($"\t {JsonConvert.SerializeObject(vertex)}");
+                }
+            }
+            Console.WriteLine();
         }
 
-        public static async Task DeleteItemAsync(string id)
+        public static async void DeleteVertexAsync(string sourceVertex)
         {
-            await client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
+            string gremlinQuery = string.Format($"g.V({sourceVertex}').drop()");
+            Console.WriteLine($"Running {gremlinQuery}");
+
+            // The CreateGremlinQuery method extensions allow you to execute Gremlin queries and iterate
+            // results asychronously
+            IDocumentQuery<Vertex> deleteVertex = client.CreateGremlinQuery<Vertex>(graph, gremlinQuery);
+            while (deleteVertex.HasMoreResults)
+            {
+                foreach (Vertex vertex in await deleteVertex.ExecuteNextAsync<Vertex>())
+                {
+                    // Since Gremlin is designed for multi-valued properties, the format returns an array. Here we just read
+                    // the first value
+                    Console.WriteLine($"\t {JsonConvert.SerializeObject(vertex)}");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static async void AddVertexEdgeAsync(string sourceVertex, string edgeLabel, string destVertext)
+        {
+            // gremlinQuery like "g.V('thomas').addE('knows').to(g.V('mary'))"
+            string gremlinQuery = string.Format($"g.V({sourceVertex}).addE({edgeLabel}).to(g.V({destVertext}))");
+            Console.WriteLine($"Running {gremlinQuery}");
+
+            // The CreateGremlinQuery method extensions allow you to execute Gremlin queries and iterate
+            // results asychronously
+            IDocumentQuery<Vertex> addEdgeVertex = client.CreateGremlinQuery<Vertex>(graph, gremlinQuery);
+            while (addEdgeVertex.HasMoreResults)
+            {
+                foreach (Vertex vertex in await addEdgeVertex.ExecuteNextAsync<Vertex>())
+                {
+                    // Since Gremlin is designed for multi-valued properties, the format returns an array. Here we just read
+                    // the first value
+                    Console.WriteLine($"\t {JsonConvert.SerializeObject(vertex)}");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static async Task<Int64> GetVertexCountAsync()
+        {
+            Int64 count = 0;
+            string gremlinQuery = "g.V().count()";
+           
+            IDocumentQuery<Int64> VertexCount = client.CreateGremlinQuery<Int64>(graph, gremlinQuery);
+            while (VertexCount.HasMoreResults)
+            {
+                foreach (Int64  vCount in await VertexCount.ExecuteNextAsync<Int64>())
+                {
+                    count = vCount;
+                    Console.WriteLine($"\t count:{vCount}");
+                }
+            }
+
+            return await Task.FromResult<Int64>(count);
+        }
+
+        public static async Task<Int64> GetEdgeCountAsync()
+        {
+            Int64 count = 0;
+            string gremlinQuery = "g.E().count()";
+
+            IDocumentQuery<Int64> EdgeCount = client.CreateGremlinQuery<Int64>(graph, gremlinQuery);
+            while (EdgeCount.HasMoreResults)
+            {
+                foreach (Int64 eCount in await EdgeCount.ExecuteNextAsync<Int64>())
+                {
+                    count = eCount;
+                    Console.WriteLine($"\t count:{eCount}");
+                }
+            }
+
+            return await Task.FromResult<Int64>(count);
         }
 
         public static void Initialize()
         {
-            client = new DocumentClient(new Uri(Endpoint), Key);
+            client = new DocumentClient(new Uri(ConfigurationManager.AppSettings["endpoint"]), ConfigurationManager.AppSettings["authKey"]);
             CreateDatabaseIfNotExistsAsync().Wait();
             CreateCollectionIfNotExistsAsync().Wait();
         }
@@ -121,7 +217,7 @@
             {
                 if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
                 {
-                    await client.CreateDocumentCollectionAsync(
+                   graph = await client.CreateDocumentCollectionAsync(
                         UriFactory.CreateDatabaseUri(DatabaseId),
                         new DocumentCollection { Id = CollectionId },
                         new RequestOptions { OfferThroughput = 400 });
